90399deba2da622f0542adf47d27a2d894b7a945
Giovane Verbinnen de Oliveira <gioverbinnen@gmail.com>
2021-08-24 16:15:40 -0300

    aula 32

--- app/Store.php
+++ app/Store.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Store extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(user::class);
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
